9092bdd53823d878effab8c17a1249fab80b32d4
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2021-08-03 13:37:26 +0900

    Add CesiumRtcExtensionComponent

---
+++ UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/CesiumRtc/CesiumRtcExtensionComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityGLTF
+{
+	public class CesiumRtcExtensionComponent : MonoBehaviour
+	{
+		[SerializeField]
+		private double centerX;
+
+		[SerializeField]
+		private double centerY;
+
+		[SerializeField]
+		private double centerZ;
+
+		public double CenterX
+		{
+			set => centerX = value;
+			get => centerX;
+		}
+
+		public double CenterY
+		{
+			set => centerY = value;
+			get => centerY;
+		}
+
+		public double CenterZ
+		{
+			set => centerZ = value;
+			get => centerZ;
+		}
+	}
+}
